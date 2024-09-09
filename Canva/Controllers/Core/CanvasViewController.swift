175e458b7bce0e483b6d24d31036ee9fcdd996d6
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-09-09 23:41:21 +0300

    Canvas ViewController which is main page added.

---
+++ Canva/Controllers/Core/CanvasViewController.swift
@@ -0,0 +1,79 @@
+//
+//  CanvasViewController.swift
+//  Canva
+//
+//  Created by Aleyna Işıkdağlılar on 9.09.2024.
+//
+
+import UIKit
+
+final class CanvasViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let canvasView: UIView = {
+        let canvasView = UIView()
+        canvasView.translatesAutoresizingMaskIntoConstraints = false
+        canvasView.backgroundColor = .white
+        return canvasView
+    }()
+    
+    private let addButton: UIButton = {
+        let addButton = UIButton()
+        addButton.setTitle("+", for: .normal)
+        addButton.backgroundColor = .blue
+        addButton.setTitleColor(.white, for: .normal)
+        addButton.layer.cornerRadius = 25
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        return addButton
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(canvasView)
+        view.addSubview(addButton)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            canvasView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            canvasView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            canvasView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            canvasView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            canvasView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            canvasView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            
+            addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            addButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            addButton.widthAnchor.constraint(equalToConstant: 50),
+            addButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    
+        let lineWidth: CGFloat = 1.0
+        let numberOfLines = 2
+        
+        for i in 1...numberOfLines {
+            let line = UIView()
+            line.backgroundColor = .gray
+            canvasView.addSubview(line)
+            line.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                line.topAnchor.constraint(equalTo: canvasView.topAnchor),
+                line.bottomAnchor.constraint(equalTo: canvasView.bottomAnchor),
+                line.leadingAnchor.constraint(equalTo: canvasView.leadingAnchor, constant: view.frame.width / 3 * CGFloat(i)),
+                line.widthAnchor.constraint(equalToConstant: lineWidth)
+            ])
+        }
+    }
+}
