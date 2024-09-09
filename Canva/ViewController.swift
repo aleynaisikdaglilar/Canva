17d73145936e48a65991aa965f1c49f72121be98
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-09-09 14:29:40 +0300

    removed storyboard

---
+++ Canva/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Canva
+//
+//  Created by Aleyna Işıkdağlılar on 9.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
